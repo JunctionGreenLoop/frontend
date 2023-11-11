4236da7d5fc37df8f9099d2dc98c8675081a82cc
kraslavchik <Slavak540@gmail.com>
2023-11-11 23:53:15 +0200

    commit

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-tab4.page',
@@ -7,8 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  public companyNameMessage: string = null;
+  public errorMessage: string = null;
+
+  constructor(
+    private http: HttpClient
+    
+  ) {}
 
   ngOnInit() {}
+
+  onClickSubmit(result: { token: string; }) {
+    this.companyNameMessage = null;
+    this.errorMessage = null;
+    this.http.get<any>("http://ec2-15-160-182-105.eu-south-1.compute.amazonaws.com:8080/api/checkCompanyByToken?token="+result.token)
+    .subscribe((res) => {
+      console.log(res);
+      this.companyNameMessage = res['company']
+
+    }, (err) => {
+      
+      console.error(err);
+      this.errorMessage = "Not Valid Token"
+    }, ) 
+ }
+
+
 
 }
